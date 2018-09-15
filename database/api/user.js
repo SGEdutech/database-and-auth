23412a9b3e98726b8199c8bdd0eb7d7ad0b76f71
Ashish Kumar <entropy.ash@gmail.com>
2018-09-16 00:13:56 +0530

    Add: User API function to respond user reviews

--- database/api/user.js
+++ database/api/user.js
@@ -2,10 +2,11 @@ const route = require('express').Router();
 const User = require('../models/user');
 const DbAPIClass = require('../api-functions');
 const userDbFunctions = new DbAPIClass(User);
-const youShallNotPass = require('../../scripts/login-check');
-const {
-	claimListing
-} = require('../database-scripts/user-claim');
+const { claimListing } = require('../database-scripts/user-claim');
+const Tuition = require('../models/tuition');
+const tuitionDbFunctions = new DbAPIClass(Tuition);
+const School = require('../models/school');
+const schoolDbFunctions = new DbAPIClass(School);
 
 route.get('/info', (req, res) => res.send(req.user));
 
@@ -18,6 +19,32 @@ route.get('/all', (req, res) => {
 		.then(data => res.send(data))
 		.catch(err => console.error(err));
 });
+
+route.get('/reviews', (req, res) => {
+	if (req.user === undefined) throw new Error('User nor logged in');
+
+	const userReviews = [];
+
+	function filterUserReviews(collections, category) {
+		collections.forEach(collection => {
+			collection.reviews.forEach(review => {
+				if (review.owner === req.user._id) {
+					review.category = category;
+					userReviews.push(review);
+				}
+			})
+		})
+	}
+
+	Promise.all([
+		tuitionDbFunctions.getAllData({ demands: 'name reviews' }),
+		schoolDbFunctions.getAllData({ demands: 'name reviews' })
+	]).then(tuitionsAndSchoolCollection => {
+		filterUserReviews(tuitionsAndSchoolCollection[0], 'tuition');
+		filterUserReviews(tuitionsAndSchoolCollection[1], 'school');
+		res.send(userReviews);
+	})
+})
 
 route.get('/', (req, res) => {
 	userDbFunctions
