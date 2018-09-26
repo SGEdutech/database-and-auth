f09268b63d43c252aa3c7b8985389c3bbfd1403d
Ashish Kumar <entropy.ash@gmail.com>
2018-09-26 11:55:33 +0530

    Remove forum comment and integrate it in forum post

--- database/models/forum-post.js
+++ database/models/forum-post.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const ForumCommentSchema = new Schema({
+	content: String,
+	user: Schema.Types.ObjectId
+})
+
 const forumPostSchema = new Schema({
 	title: { type: String, required: true },
 	content: { type: String, required: true },
-	comments: [{ type: Schema.Types.ObjectId, ref: 'forumComment' }]
+	comments: [ForumCommentSchema]
 });
 
 const ForumPost = mongoose.model('forumPost', forumPostSchema);
