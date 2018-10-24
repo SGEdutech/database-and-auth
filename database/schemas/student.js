2724131fba686f538d6040286891d44065b0ec4b
Ashish Kumar <entropy.ash@gmail.com>
2018-10-24 13:22:04 +0530

    Change student module

--- database/schemas/student.js
+++ database/schemas/student.js
@@ -2,6 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const { required } = require('../../config.json').MONGO;
 
+const PaymentSchema = new Schema({
+	courseId: Schema.Types.ObjectId,
+	batchId: Schema.Types.ObjectId,
+	discountAmount: Number,
+	discountReason: String,
+	feeCollected: Number,
+	modeOfPayment: String,
+	bank: String,
+	dateOfCheck: String,
+	checkNumber: String,
+	cardNumber: String,
+	transactionId: String,
+	nextInstallment: Date
+});
+
 const StudentSchema = new Schema({
 	eduatlasId: String,
 	rollNumber: { type: String, required },
@@ -9,13 +24,7 @@ const StudentSchema = new Schema({
 	email: { type: String, required },
 	address: String,
 	contactNumber: String,
-	courseId: Schema.Types.ObjectId,
-	batchId: Schema.Types.ObjectId,
-	discountAmount: Number,
-	discountReason: String,
-	feeCollected: Number,
-	modeOfPayment: String,
-	nextInstallment: Date
+	payments: [PaymentSchema]
 });
 
 module.exports = StudentSchema;
