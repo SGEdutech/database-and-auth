586681f4505133085a88b0f90bf6158efe28e484
Ashish Kumar <entropy.ash@gmail.com>
2018-11-26 11:23:02 +0530

    Fix: Field courseId in payment schema accepting all string

--- database/schemas/student.js
+++ database/schemas/student.js
@@ -14,7 +14,7 @@ const InstallmentSchema = new Schema({
 });
 
 const PaymentSchema = new Schema({
-	courseId: { type: String, lowercase: true },
+	courseId: { type: Schema.Types.ObjectId, lowercase: true },
 	courseFee: { type: Number, required },
 	discountAmount: Number,
 	discountReason: String,
