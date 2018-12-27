1c35095e722998df656d2c5b9152c2892e72ef09
16bitash <entropy.ash@gmail.com>
2018-12-27 11:19:14 +0530

    Fix: Minor fix in forum schema

--- database/schemas/forum.js
+++ database/schemas/forum.js
@@ -5,14 +5,14 @@ const { required, select } = require('../../config.json').MONGO;
 const ForumComments = new Schema({
 	userEmail: String,
 	content: String,
-	createdAt: { type: Date, default: Date.now() }
+	createdAt: { type: Date, default: Date.now }
 });
 
 const ForumSchema = new Schema({
 	title: { type: String, required },
 	body: { type: String, required },
 	comments: [ForumComments],
-	createdAt: { type: Date, default: Date.now() }
+	createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = ForumSchema;
