e9fe15c03449d601d2fbcee78576d04d9e93cb05
16bitash <entropy.ash@gmail.com>
2019-03-09 16:45:41 +0530

    Surround all firebase functions in try catch block

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -10,51 +10,71 @@ const config = {
 };
 
 async function addRegestrationIdToGroup(notificationKey, notificationKeyName, registrationToken) {
-	const data = {
-		operation: 'add',
-		notification_key: notificationKey,
-		notification_key_name: notificationKeyName,
-		registration_ids: [registrationToken]
-	};
-	const response = await axios.post('https://android.googleapis.com/gcm/notification', data, config);
-	return response.data;
+	try {
+		const data = {
+			operation: 'add',
+			notification_key: notificationKey,
+			notification_key_name: notificationKeyName,
+			registration_ids: [registrationToken]
+		};
+		const response = await axios.post('https://android.googleapis.com/gcm/notification', data, config);
+		return response.data;
+	} catch (error) {
+		console.error(error);
+	}
 }
 
 async function createNotificationGroup(notificationKeyName, registrationToken) {
-	const data = {
-		operation: 'create',
-		notification_key_name: notificationKeyName,
-		registration_ids: [registrationToken]
-	};
-	const response = await axios.post('https://android.googleapis.com/gcm/notification', data, config);
-	return response.data;
+	try {
+		const data = {
+			operation: 'create',
+			notification_key_name: notificationKeyName,
+			registration_ids: [registrationToken]
+		};
+		const response = await axios.post('https://android.googleapis.com/gcm/notification', data, config);
+		return response.data;
+	} catch (error) {
+		console.error(error);
+	}
 }
 
 async function getNotificationKey(notificationKeyName) {
-	const response = await axios.get(`https://fcm.googleapis.com/fcm/notification?notification_key_name=${notificationKeyName}`, config);
-	return response.data;
+	try {
+		const response = await axios.get(`https://fcm.googleapis.com/fcm/notification?notification_key_name=${notificationKeyName}`, config);
+		return response.data;
+	} catch (error) {
+		console.error(error);
+	}
 }
 
 async function removeRegestrationIdFromGroup(notificationKey, notificationKeyName, registrationToken) {
-	const data = {
-		operation: 'remove',
-		notification_key: notificationKey,
-		notification_key_name: notificationKeyName,
-		registration_ids: [registrationToken]
-	};
-	const response = await axios.post('https://android.googleapis.com/gcm/notification', data, config);
-	return response.data;
+	try {
+		const data = {
+			operation: 'remove',
+			notification_key: notificationKey,
+			notification_key_name: notificationKeyName,
+			registration_ids: [registrationToken]
+		};
+		const response = await axios.post('https://android.googleapis.com/gcm/notification', data, config);
+		return response.data;
+	} catch (error) {
+		console.error(error);
+	}
 }
 
 async function sendNotificationToAGroup(body, notificationKeyName, title) {
-	const { notification_key: notificationKey } = await getNotificationKey(notificationKeyName);
-	const data = {
-		priority: 'HIGH',
-		notification: { title, body },
-		to: [notificationKey]
-	};
-	const response = await axios.post('https://fcm.googleapis.com/fcm/send', data, config);
-	return response.data;
+	try {
+		const { notification_key: notificationKey } = await getNotificationKey(notificationKeyName);
+		const data = {
+			priority: 'HIGH',
+			notification: { title, body },
+			to: [notificationKey]
+		};
+		const response = await axios.post('https://fcm.googleapis.com/fcm/send', data, config);
+		return response.data;
+	} catch (error) {
+		console.error(error);
+	}
 }
 
 async function shoveRegistrationIdInAGroup(notificationKeyName, registrationToken) {
